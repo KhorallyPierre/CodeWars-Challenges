aba71dbb3af872ea2f57607202664e6ef66a605a
Khorally Pierre <KhorallyPierre@users.noreply.github.com>
2021-02-28 15:43:22 -0500

    week2 day 5 and 7

--- week2/week2Day5.js
+++ week2/week2Day5.js
@@ -13,3 +13,90 @@ function warnTheSheep(queue) {
     return `Oi! Sheep number ${queue.length - index - 1}! You are about to be eaten by a wolf!`;
   }
 }
+
+
+//1
+function warnTheSheep(queue) {
+ if (queue[queue.length -1] === 'wolf'){
+   return 'Pls go away and stop eating my sheep';
+ } else {
+   let index = queue.findIndex( (x) => x == 'wolf');
+   return `Oi Sheep number ${queue.length - index - 1}! You are about to be eaten by a wolf!`;
+ }
+}
+
+//2
+function warnTheSheep(queue) {
+  if (queue[queue.length -1] === 'wolf'){
+    return 'Pls go away and stop eating my sheep';
+  } else {
+    let index = queue.findIndex((x) => x == 'wolf');
+  } return `oi sheep number ${queue.length - index - 1}! Your  aboutto be eaten by a wolf!`;
+}
+
+//3
+function warnTheSheep(queue) {
+  if (queue[queue.length -1] === 'wolf'){
+    return 'pls go away and stop eating my sheep';
+  } else {
+    let index = queue.findIndex( (x) => x == 'wolf');
+  } return `oi sheep number ${queue.length - index - 1 } black black black!`
+}
+
+//4
+function warnTheSheep(queue) {
+  if (queue[queue.length -1] === 'wolf'){
+    return 'pls go away and stop eating my sheep';
+  } else {
+    let index = queue.findIndex((x) => x == 'wolf')
+  } return `oi sheep number ${queue.length - index - 1 } black black black!`;
+}
+//5
+function warnTheSheep(queue) {
+  if (queue[queue.length -1] === 'wolf'){
+    return 'pls go away and stop eating my sheep';
+  } else {
+    let index = queue.findIndex((x) => == 'wolf')
+  } return `oi sheep number ${queue.length - index - 1} black black blak!`;
+}
+//6
+function warnTheSheep(queue) {
+  if (queue[queue.length -1] === 'wolf'){
+    return 'pls go away and stop eating my sheep';
+  } else {
+    let index = queue.findIndex((x) => == 'wolf')
+  } return `oi sheep number ${queue.length - index - 1} ha hah hah hahah! `;
+}
+//7
+function warnTheSheep(queue) {
+  if (queue[queue.length -1] === 'wolf'){
+    return 'pls go away and stop eating my sheep';
+  } else {
+    let index = queue.findIndex((x) => == 'wolf')
+  } return `oi sheep number ${queue.length - index - 1} ha hahah haha haha `;
+}
+//8
+function warnTheSheep(queue) {
+  if (queue[queue.length -1] === 'wolf'){
+    return 'pls go away and stop eating my sheep;'
+  } else {
+    let index = queue.findIndex((x) => == 'wolf')
+  } return  `oi sheep number ${queue.length - index - 1} hahahahahah`;
+}
+
+//9
+function warnTheSheep(queue) {
+  if (queue[queue.length -1] === 'wolf'){
+    return 'pls go away and stop eating my sheep;'
+  } else {
+    let index = queue.findIndex((x) => == 'wolf')
+  } return `oi sheep number ${queue.length -index - 1} hahahahahha `;
+}
+//10
+function warnTheSheep(queue) {
+  if (queue[queue.length -1] === 'wolf'){
+    return 'pls go away and stop eating my sheep;'
+  } else {
+    let index = queue.findIndex((x) => == 'wolf')
+  } return `oi sheep number ${queue.length - index - 1 } hha haha hahah`;
+}

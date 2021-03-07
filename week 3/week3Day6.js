67677276ce974b6ffa3013486a03c28365915423
Khorally Pierre <KhorallyPierre@users.noreply.github.com>
2021-03-06 23:04:01 -0500

    week3 day 6 codewars

---
+++ week 3/week3Day6.js
@@ -0,0 +1,9 @@
+// Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+function add(a, b){
+	if (a == b) {
+    return true
+  } else {
+    return false
+  }
+}

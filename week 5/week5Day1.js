f06ed81d0259bafe220cd4acfc130f1d9bdd10be
Khorally Pierre <KhorallyPierre@users.noreply.github.com>
2021-03-15 22:48:41 -0400

    week 5 day 1 codewars

---
+++ week 5/week5Day1.js
@@ -0,0 +1,7 @@
+// Ask a small girl - "How old are you?". She always says strange things... Lets help her!
+//
+// For correct answer program should return int from 0 to 9.
+
+function getAge(inputString){
+return parseInt(inputString)
+}

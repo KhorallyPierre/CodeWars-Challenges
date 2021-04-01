3c82ced383eca89d7d638d96590c725c3f15c9e7
Khorally Pierre <kvpierre@gmail.com>
2021-03-31 22:40:08 -0400

    day 3

---
+++ week 7/week7Day3.js
@@ -0,0 +1,12 @@
+// A worm is climbing up a bamboo. How long it will be continuing, if every day it`s climbing up the bamboo "x" amount of meters every day, and each night it slides down for "y" meters?
+
+// In this kata you have height, up meters, down meters. Write the function, that calculate and return "days".
+//
+// function climbingTime(height, up, down)
+
+function climbingTime(height, up, down) {
+  if (up <= 0 || down < 0 || height <= 0) return false
+   for (var i=up,j=0;;i+=up-down,j++){
+   if (i>=height) return j+1
+ }
+}

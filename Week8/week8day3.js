b2cd41b42322379dc485196d0b08cb0c93bc5862
Khorally Pierre <kvpierre@gmail.com>
2021-04-07 21:21:40 -0400

    codewars day 3 week 8

---
+++ Week8/week8day3.js
@@ -0,0 +1,8 @@
+// The function is not returning the correct values. Can you figure out why?
+// 
+// Example (Input --> Output ):
+
+function getPlanetName(id){
+  var names = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptun'];
+  return names[id - 1];
+}

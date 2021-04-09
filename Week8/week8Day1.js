a814065ae62d4400dd8b814fc1360367979a18eb
Khorally Pierre <kvpierre@gmail.com>
2021-04-09 13:18:34 -0400

    updates

--- Week8/week8Day1.js
+++ Week8/week8Day1.js
@@ -1,4 +1,7 @@
 This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
 
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
 
-https://github.com/Automedon/CodeWars-7-kyu-Soluitions-part-2
+(use assigment = not ===)

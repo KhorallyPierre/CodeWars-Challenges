5a11137666071fc499c6b9d81155bbe0b74481fd
Khorally Pierre <kvpierre@gmail.com>
2021-04-22 11:10:06 -0400

    codewars week 10

---
+++ week10/musketeers.js
@@ -0,0 +1,14 @@
+let arr = ["Athos", "Porthos", "Atamis"];
+
+for (let i = 0; i<= arr.length;i++){
+  console.log(arr[i])
+}
+arr.pop()
+arr.push( "D'Artagnan")
+
+
+arr.forEach(element => console.log(element)
+
+for (let ar of arr){
+  console.log(arr)
+}

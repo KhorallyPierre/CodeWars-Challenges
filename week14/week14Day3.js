2ada50557ff7fa0e9eddd485998931ca29512ba5
Khorally Pierre <kvpierre@gmail.com>
2021-05-19 22:21:42 -0400

    day 3 codewars

---
+++ week14/week14Day3.js
@@ -0,0 +1,27 @@
+// Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+//
+
+
+var mergeTwoLists = function(l1, l2) {
+  let newArray = []
+  let i = 0
+  let j = 0
+
+  while (i < l1.length && j < l2.length) {
+    if (l1[i] < l2[j]) {
+      newArray.push(l1[i])
+      i++
+      newArray.push(l2[j])
+    } else if (l2[j] < l1[i]) {
+      newArray.push(l2[j])
+      j++
+      newArray.push(l1[i])
+    } else {
+      newArray.push(l1[i])
+      newArray.push(l2[j])
+      i++
+      j++
+    }
+  };
+  return newArray
+}

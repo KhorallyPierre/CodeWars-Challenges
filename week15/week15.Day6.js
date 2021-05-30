cd088f3e69d785951ff2da7848465651b66c8ced
Khorally Pierre <kvpierre@gmail.com>
2021-05-29 20:00:00 -0600

    stop snacking

---
+++ week15/week15.Day6.js
@@ -0,0 +1,9 @@
+// Create a function that grabs the number of snacks from
+// the input and tells you to stop that many times
+
+function stopSnacking(snacks){
+  for (let i =0; i < snacks.length; i ++){
+    return'stop snacking!!'
+  }
+}
+console.log(stopSnacking(20))

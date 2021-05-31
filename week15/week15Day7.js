4d39e74a4b5507062df778caed75e38ba64613a8
Khorally Pierre <kvpierre@gmail.com>
2021-05-30 20:00:00 -0600

    day 7 codewars

---
+++ week15/week15Day7.js
@@ -0,0 +1,7 @@
+// create a function that subtracts two numbers and alerts the difference
+
+function subtract(num1, num2){
+  let difference = num1-num2
+  console.log(difference)
+}
+subtract(30, 10)

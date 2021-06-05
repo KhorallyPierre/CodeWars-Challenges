bdf4dc638b3cf2a96013c10946162e2eb25c5a42
Khorally Pierre <kvpierre@gmail.com>
2021-06-04 23:53:35 -0400

    junne 4 codewar

---
+++ June/june4.js
@@ -0,0 +1,8 @@
+// Create a function that subtracts 4 numbers and alerts the difference
+
+
+function subtract(num1, num2, num3, num4){
+  let division = num1 - num2 - num3 - num4
+  return division
+}
+console.log(subtract(10,8,2,1))
